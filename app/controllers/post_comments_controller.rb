0670c7726713cef03b7f31e7db8053e15e5fb254
horiren <138359070+horiren@users.noreply.github.com>
2023-08-02 06:28:09 +0000

    ouyoukadai6-1

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,15 @@
 class PostCommentsController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book.id)
   end
   
   def destroy
+    @book = Book.find(params[:book_id])
     PostComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
   end
 
   private
